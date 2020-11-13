542ca6e83984b634d7102faea2814eca50afbc4a
Arian Sharif <arian.sharif1606@gmail.com>
2020-11-13 07:31:24 -0700

    created a file for discussions javascript changed discussions.html

---
+++ scripts/1800_database.js
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------
+// Your web app's Firebase configuration;
+// Specifies which firebase project your application is connected with.
+//---------------------------------------------------------------------
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAR8HgIqxHXeLJPff14h2ArTbILdVLEVrI",
+  authDomain: "database-1800.firebaseapp.com",
+  databaseURL: "https://database-1800.firebaseio.com",
+  projectId: "database-1800",
+  storageBucket: "database-1800.appspot.com",
+  messagingSenderId: "741786407495",
+  appId: "1:741786407495:web:fd836f335d2471e80a5762"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+  // Create the Firestore database object
+  // Henceforce, any reference to the database can be made with "db"
+  const db = firebase.firestore();
---
+++ scripts/discussions.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $(".btn-primary").css({"background": "#6351ce","border-color": "#6351ce"});
+    $("#searchButton").css("margin-left", "1rem");
+    $(".searchContainer").css("justify-content", "center");
+
+
+    $('#searchButton').on("click", async function(){
+        var discussionId = document.getElementById("search-bar").value;
+        
+        var arr = discussionId.split(' ');
+        for(var i = 0; i < arr.length; i++){
+            var word = arr[i];
+            console.log(word);
+            
+            var thread = await db.collection('discussions').doc(word).get();
+
+            if(thread.exists){
+                thread.then(function(q){
+                q.data()['num']
+            
+                });
+            }
+            
+            const incrementNum = firebase.firestore.FieldValue.increment(1);
+            await db.collection('discussions').doc(word).update({
+                num: incrementNum
+            })
+        }
+        
+    })
+
+    $('#createButton').on("click", async function(){ 
+
+        var threads = await db.collection('discussions');
+        var name = document.getElementById("exampleInputName").value;
+
+        threads.doc(name).set({
+            
+            views : 0,
+            textInside : document.getElementById("TextArea").value
+        })
+
+    })
+
+    
+    
+
+    
+    });
