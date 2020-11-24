5ea3824ae592d48f0f5d6ec44e8b047c7fef25d6
Arian Sharif <arian.sharif1606@gmail.com>
2020-11-24 10:48:12 -0700

    Fixed the delete buttons for the reminders section

--- scripts/reminders.js
+++ scripts/reminders.js
@@ -1,5 +1,6 @@
-function deleted() {
-    db.collection("reminders").doc(name).delete().then(function () {
+
+function deleted(doc) {
+    db.collection("reminders").doc(doc).delete().then(function () {
         console.log("Deleted");
     }).catch (function(error) {
         console.error("Error deleting: ", error);
@@ -13,25 +14,28 @@ function getReminders() {
             snap.forEach(function(doc) {
                 console.log(doc.data());
                 var name = doc.data().Name;
+                
                 var description = doc.data().Description;
                 var d1 = $("#addCard").prepend(
                     "<div class='card' style='width: 100%; height: 100%'>" 
                     +   "<h5 class='card-header'>" + name + "</h5>"
                     +   "<div class='card-body'>"
                     +      "<p class='card-text'>" + description + "</p>"
-                    +      "<button type='button' class='btn btn-danger' onclick='deleted()'>Delete</button>"
-                    +   "</div"
-                    +"</div)");
+                    +      "<button type='button' class='btn btn-danger' onClick=deleted('" +name.split(' ').join('') +"') >Delete</button>"
+                    +   "</div>"
+                    +"</div)>");
         })
     })
 }
+
+
 
 function addReminder() {
 
     var reminders = db.collection("reminders");
     var name = document.getElementById("reminderName").value;
 
-    db.collection("reminders").doc(name).set({
+    db.collection("reminders").doc(name.split(' ').join('')).set({
         Name: name,
         Description: document.getElementById("reminderDscrpt").value
     })
