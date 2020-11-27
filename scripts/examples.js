fd0be4649855423e3b5f8270362ca98ae9682e68
Arian Sharif <arian.sharif1606@gmail.com>
2020-11-27 09:36:55 -0700

    got rid of profile

--- scripts/examples.js
+++ scripts/examples.js
@@ -1,23 +1,23 @@
 // Editing some css using JQuery, more of a practice for COMP 1537
 
-$(document).ready(function () {
+$(document).ready(function() {
     setTimeout(function () {
         $("#exampleModal").modal('show');
     }, 2000);
 
-    $("#exampleModalLong").click(function () {
+    $("#exampleModalLong").click(function (){
         $("#exampleModalLong").modal('show');
     });
 
-    $("#exampleModalLong2").click(function () {
+    $("#exampleModalLong2").click(function (){
         $("#exampleModalLong2").modal('show');
     });
 
-    $("#exampleModalLong3").click(function () {
+    $("#exampleModalLong3").click(function (){
         $("#exampleModalLong3").modal('show');
     });
-});
+  });
 
-$(function () {
+  $(function () {
     $('[data-toggle="popover"]').popover()
-})
+  })
