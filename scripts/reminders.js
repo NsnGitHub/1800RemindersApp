5f4fe426ba1b9f7b6c5ff446e6b929718982edaf
Anson Chan <achan436@my.bcit.ca>
2020-12-03 18:16:48 -0800

    backup

--- scripts/reminders.js
+++ scripts/reminders.js
@@ -1,10 +1,12 @@
 
 function deleted(doc) {
-    db.collection("reminders").doc(doc).delete().then(function () {
-        console.log("Deleted");
-    }).catch (function(error) {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("users").doc(user.uid).collection("reminders").doc(doc).delete().then(function () {
+            console.log("Deleted");
+        }).catch (function(error) {
         console.error("Error deleting: ", error);
-    })
+        })
 
     var modal = $("#addCard").append(
         '<div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">'
@@ -25,26 +27,28 @@ function deleted(doc) {
         +        '</div>'
         +    '</div>'
         +'</div>');
-
+    })
 }
 
 function getReminders() {
-    db.collection("reminders")
-        .get()
-        .then(function(snap) {
-            snap.forEach(function(doc) {
-                console.log(doc.data());
-                var name = doc.data().Name;
-                
-                var description = doc.data().Description;
-                var d1 = $("#addCard").prepend(
-                    "<div class='card' style='width: 100%; height: 100%'>" 
-                    +   "<h5 class='card-header'>" + name + "</h5>"
-                    +   "<div class='card-body'>"
-                    +      "<p class='card-text'>" + description + "</p>"
-                    +      "<button type='button' class='btn btn-danger' data-toggle='modal' data-target='#exampleModalCenter1' onClick=deleted('" +name.split(' ').join('') +"') >Delete</button>"
-                    +   "</div>"
-                    +"</div)>");
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("users").doc(user.uid).collection("reminders")
+            .get()
+            .then(function(snap) {
+                snap.forEach(function(doc) {
+
+                    var name = doc.data().Name;
+                    var description = doc.data().Description;
+
+                    var d1 = $("#addCard").prepend(
+                        "<div class='card' style='width: 100%; height: 100%'>" 
+                        +   "<h5 class='card-header'>" + name + "</h5>"
+                        +   "<div class='card-body'>"
+                        +      "<p class='card-text'>" + description + "</p>"
+                        +      "<button type='button' class='btn btn-danger' data-toggle='modal' data-target='#exampleModalCenter1' onClick=deleted('" +name.split(' ').join('') +"') >Delete</button>"
+                        +   "</div>"
+                        +"</div)>");
+            })
         })
     })
 }
@@ -53,35 +57,36 @@ function getReminders() {
 
 function addReminder() {
 
-    var reminders = db.collection("reminders");
-    var name = document.getElementById("reminderName").value;
+    firebase.auth().onAuthStateChanged(function (user) {
+        var reminders = db.collection("users").doc(user.uid).collection("reminders");
+        var name = document.getElementById("reminderName").value;
 
-    db.collection("reminders").doc(name.split(' ').join('')).set({
-        Name: name,
-        Description: document.getElementById("reminderDscrpt").value
-    })
+        reminders.doc(name.split(' ').join('')).set({
+            Name: name,
+            Description: document.getElementById("reminderDscrpt").value
+        })
 
-    var modal = $("#addCard").append(
-        '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">'
-        +    '<div class="modal-dialog modal-dialog-centered" role="document">'
-        +        '<div class="modal-content">'
-        +           '<div class="modal-header">'
-        +               '<h5 class="modal-title" id="exampleModalLongTitle">Success!</h5>'
-        +              '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
-        +                  '<span aria-hidden="true">&times;</span>'
-        +               '</button>'
-        +           '</div>'
-        +           '<div class="modal-body">'
-        +               'You have successfully added a reminder. Refresh the page to update.'
-        +           '</div>'
-        +           '<div class="modal-footer">'
-        +               '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'
-        +            '</div>'
-        +        '</div>'
-        +    '</div>'
-        +'</div>');
+        var modal = $("#addCard").append(
+            '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">'
+            +    '<div class="modal-dialog modal-dialog-centered" role="document">'
+            +        '<div class="modal-content">'
+            +           '<div class="modal-header">'
+            +               '<h5 class="modal-title" id="exampleModalLongTitle">Success!</h5>'
+            +              '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+            +                  '<span aria-hidden="true">&times;</span>'
+            +               '</button>'
+            +           '</div>'
+            +           '<div class="modal-body">'
+            +               'You have successfully added a reminder. Refresh the page to update.'
+            +           '</div>'
+            +           '<div class="modal-footer">'
+            +               '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'
+            +            '</div>'
+            +        '</div>'
+            +    '</div>'
+            +'</div>');
+    })       
 }
-
 
 getReminders();
 
